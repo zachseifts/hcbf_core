bf48244c06e4dac487c63280e5d654e585175f6e
Zach Seifts <zach.seifts@gmail.com>
2014-02-12 14:20:31 -0500

    Adding in basic structure of the site

---
+++ templates/hcbf_about.tpl.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-sm-6">
+    <p>This is left side of the about page</p>
+  </div>
+  <div class="col-sm-6">
+    <p>This is the right side of the about page</p>
+  </div>
+</div>
