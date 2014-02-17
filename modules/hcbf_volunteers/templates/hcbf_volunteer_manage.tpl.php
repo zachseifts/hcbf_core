a770c568ff96ff32f2ebba5fca08a0683045c709
Zach Seifts <zach.seifts@gmail.com>
2014-02-17 12:22:46 -0500

    Adding in fields and working on the functionality

---
+++ modules/hcbf_volunteers/templates/hcbf_volunteer_manage.tpl.php
@@ -0,0 +1,24 @@
+<div class="row">
+  <div class="col-sm-12">
+    <table class="table table-responsive table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Accepted?</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($volunteers as $volunteer) { ?>
+          <tr>
+            <td><?php print render($volunteer['name']); ?> (<?php print render($volunteer['email']); ?>)</td>
+            <td><?php print render($volunteer['phone']); ?></td>
+            <td>Yes</td>
+            <td>No</td>
+          </tr>
+        <?php }; ?>
+      </tbody>
+    </table>
+  </div>
+</div>
