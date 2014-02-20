b8344e91bf084b00e4161fc965e2dcd0c9a891d3
Zach Seifts <zach.seifts@gmail.com>
2014-02-20 10:12:35 -0500

    Adding in block and admin options

--- modules/hcbf_volunteers/templates/hcbf_volunteer_manage.tpl.php
+++ modules/hcbf_volunteers/templates/hcbf_volunteer_manage.tpl.php
@@ -1,33 +1,36 @@
 <div class="row">
   <div class="col-sm-12">
-    <table class="table table-responsive">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Phone</th>
-          <th>Accepted</th>
-          <th>Shift</th>
-          <th>Job</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-          <th>&nbsp;</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($volunteers as $volunteer) { ?>
-          <tr class="<?php print $volunteer['row_class']; ?>">
-            <td><?php print render($volunteer['name']); ?></td>
-            <td><?php print render($volunteer['phone']); ?></td>
-            <td><?php print render($volunteer['approved']); ?></td>
-            <td><?php print render($volunteer['shift']); ?></td>
-            <td><?php print render($volunteer['job']); ?></td>
-            <td><?php print render($volunteer['email']); ?></td>
-            <td><?php print render($volunteer['view']); ?></td>
-            <td><?php print render($volunteer['edit']); ?></td>
-            <td><?php print render($volunteer['delete']); ?></td>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Accepted</th>
+            <th>Shift</th>
+            <th>Job</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
           </tr>
-        <?php }; ?>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          <?php foreach ($volunteers as $volunteer) { ?>
+            <tr class="<?php print $volunteer['row_class']; ?>">
+              <td><?php print render($volunteer['name']); ?></td>
+              <td><?php print render($volunteer['phone']); ?></td>
+              <td><?php print render($volunteer['approved']); ?></td>
+              <td><?php print render($volunteer['shift']); ?></td>
+              <td><?php print render($volunteer['job']); ?></td>
+              <td><?php print render($volunteer['email']); ?></td>
+              <td><?php print render($volunteer['view']); ?></td>
+              <td><?php print render($volunteer['edit']); ?></td>
+              <td><?php print render($volunteer['delete']); ?></td>
+            </tr>
+          <?php }; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
