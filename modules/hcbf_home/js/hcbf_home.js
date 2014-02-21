52a4da222bc14918e910f530d5d1e089f38133d0
Zach Seifts <zach.seifts@gmail.com>
2014-02-21 11:19:06 -0500

    setting up the homepage module

---
+++ modules/hcbf_home/js/hcbf_home.js
@@ -0,0 +1,12 @@
+(function($) {
+  Drupal.behaviors.hcbf_home = {
+    attach: function(context, settings) {
+      var skroller = skrollr.init({
+        forceHeight: false,
+        render: function(data) {
+          console.log(data.curTop);
+        }
+      });
+    }
+  }
+})(jQuery);
---
+++ modules/hcbf_home/templates/hcbf_home.tpl.php
@@ -0,0 +1,40 @@
+<section id="slide-1">
+  <div class="bcg"
+       data-center="background-position: 50% 0px;",
+       data-top-bottom="background-position: 50% -100px",
+       data-anchor-target="#slide-1">
+    <div class="row slide-container">
+      <div class="col-sm-12 hcbfContent"
+           data-center="opacity: 1;"
+           data-106-top="opacity: 0;"
+           data-anchor-top="#slide-1 h2">
+        <h2>Praesent posuere a urna sed ultrices. Duis sed ipsum semper orci consectetur pulvinar.</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="slide-2">
+  <div class="bcg"
+       data-center="background-position: 50% 0px;",
+       data-top-bottom="background-position: 50% -100px",
+       data-anchor-target="#slide-2">
+    <div class="row slide-container dark">
+      <div class="col-sm-12"
+           data-center="opacity: 1;"
+           data-106-top="opacity: 0;"
+           data-anchor-top="#slide-2 h2">
+        <h2>Duis sed ipsum semper orci consectetur pulvinar.</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="row">
+  <div class="col-sm-6">
+    <p>This is left side of the homepage</p>
+  </div>
+  <div class="col-sm-6">
+    <p>This is the right side of the homepage</p>
+  </div>
+</div>
