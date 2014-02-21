a59894fc1d781cf3fb8d96ede06028c197058bee
Zach Seifts <zach.seifts@gmail.com>
2014-02-21 12:28:58 -0500

    removing menu structure from the core module

--- templates/hcbf_about.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-  <div class="col-sm-6">
-    <p>This is left side of the about page</p>
-  </div>
-  <div class="col-sm-6">
-    <p>This is the right side of the about page</p>
-  </div>
-</div>
