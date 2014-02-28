dfae75c4d03aefc6f93f375e9d85e8a64e57fe72
Zach Seifts <zach.seifts@gmail.com>
2014-02-28 11:01:45 -0500

    Working on the breweries page

--- modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
@@ -8,7 +8,10 @@
   <div class="row">
     <?php foreach ($breweries_row as $brewery) { ?>
       <div class="col-sm-4">
+        <img src="http://placehold.it/220x220" />
         <?php print render($brewery['title']); ?>
+        <?php print render($brewery['location']); ?>
+        <?php print render($brewery['website']); ?>
       </div>
     <?php }; ?>
   </div>
