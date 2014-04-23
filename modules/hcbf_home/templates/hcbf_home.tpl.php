cb9e4270872e69be31eba9d88fcf49e4fe06e501
Zach Seifts <zach.seifts@gmail.com>
2014-04-23 09:45:04 -0400

    starting to add in the homepage

--- modules/hcbf_home/templates/hcbf_home.tpl.php
+++ modules/hcbf_home/templates/hcbf_home.tpl.php
@@ -1,40 +1,31 @@
-<section id="slide-1">
-  <div class="bcg"
-       data-center="background-position: 50% 0px;",
-       data-top-bottom="background-position: 50% -100px",
-       data-anchor-target="#slide-1">
-    <div class="row slide-container">
-      <div class="col-sm-12 hcbfContent"
-           data-center="opacity: 1;"
-           data-106-top="opacity: 0;"
-           data-anchor-top="#slide-1 h2">
-        <h2>Praesent posuere a urna sed ultrices. Duis sed ipsum semper orci consectetur pulvinar.</h2>
+
+<div id="get-ready-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1>Get Ready</h1>
+        <h2>Summer 2014</h2>
+        <h3>High Country Beer Fest</h3>
       </div>
     </div>
   </div>
-</section>
+</div>
 
-<section id="slide-2">
-  <div class="bcg"
-       data-center="background-position: 50% 0px;",
-       data-top-bottom="background-position: 50% -100px",
-       data-anchor-target="#slide-2">
-    <div class="row slide-container dark">
-      <div class="col-sm-12"
-           data-center="opacity: 1;"
-           data-106-top="opacity: 0;"
-           data-anchor-top="#slide-2 h2">
-        <h2>Duis sed ipsum semper orci consectetur pulvinar.</h2>
+<div id="meta-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-4">
+        <div class="well well-lg">
+          <h1>Title 1</h1>
+          <p>words words words words</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <h1>Title 1</h1>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <h1>Title 1</h1>
       </div>
     </div>
-  </div>
-</section>
-
-<div class="row">
-  <div class="col-sm-6">
-    <p>This is left side of the homepage</p>
-  </div>
-  <div class="col-sm-6">
-    <p>This is the right side of the homepage</p>
   </div>
 </div>
