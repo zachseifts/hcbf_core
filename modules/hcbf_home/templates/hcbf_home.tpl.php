4a92b9aac2879ca7d34c78246b00f5c18190e2c2
Zach Seifts <zach.seifts@gmail.com>
2014-05-21 13:57:56 -0400

    Updating the homepage

--- modules/hcbf_home/templates/hcbf_home.tpl.php
+++ modules/hcbf_home/templates/hcbf_home.tpl.php
@@ -1,31 +1,56 @@
 
-<div id="get-ready-wrapper">
+<div id="meta-wrapper" class="wrapper">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12">
-        <h1>Get Ready</h1>
-        <h2>Summer 2014</h2>
-        <h3>High Country Beer Fest</h3>
+      <div class="col-xs-12 col-sm-4">
+        <div class="text-center">
+          <i class="fa fa-ticket fa-6x"></i>
+        </div>
+        <h1 class="text-center">Buy Tickets</h1>
+        <p class="lead text-center">Tickets will be on sale soon, check back for dates!</p>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <div class="text-center">
+          <i class="fa fa-beer fa-6x"></i>
+        </div>
+        <h1 class="text-center">The Festivities</h1>
+        <p class="lead text-center">Join us at the High Country Fair Grounds on Saturday, August 30, 2014 from 3:00pm to 7:00 pm to try a varity of craft beer, craft food, educational seminars, and live music.</p>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <div class="text-center">
+          <i class="fa fa-users fa-6x"></i>
+        </div>
+        <h1 class="text-center">Get involved</h1>
+        <p class="lead text-center">We're looking for volunteers to help us put on the 7<sup>th</sup> annual High Country Beer Fest.</p>
+        <div class="text-center">
+          <p>
+            <a class="btn btn-success btn-lg" href="/volunteer" role="button">
+             <i class="fa fa-thumbs-up"></i> I'd like to get involved
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    
+<hr />
+
+<div id="about-wrapper" class="wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-7">
+        <h1 class="heading">About the High Country Beer Fest</h1>
+        <p class="lead">The High Country Beer Fest is a celebration of craft beer from around the region and the world. Come sample outstanding beers and delicious food in the beautiful high country of North Carolina. Educational seminars will cover aspects of beer tasting, brewing, and food pairing.</p>
+      </div>
+      <div class="col-xs-12 col-md-5">
+        <h1 class="heading">Want to serve your beer?</h1>
+        <p class="lead">We're looking for craft breweries who want to participate in this year's High Country Beer Fest.</p>
+        <p>
+          <a class="btn btn-success btn-lg" href="/breweries/register" role="button">I'd like to register my brewery</a>
+        </p>
       </div>
     </div>
   </div>
 </div>
 
-<div id="meta-wrapper">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-4">
-        <div class="well well-lg">
-          <h1>Title 1</h1>
-          <p>words words words words</p>
-        </div>
-      </div>
-      <div class="col-xs-12 col-sm-4">
-        <h1>Title 1</h1>
-      </div>
-      <div class="col-xs-12 col-sm-4">
-        <h1>Title 1</h1>
-      </div>
-    </div>
-  </div>
-</div>
