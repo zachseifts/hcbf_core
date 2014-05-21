1784b9a856024ccdfc4faf9a710859c6c9ca1d71
Zach Seifts <zach.seifts@gmail.com>
2014-05-21 14:19:05 -0400

    wrapping h1 elements in .page-header classes

--- modules/hcbf_home/templates/hcbf_home.tpl.php
+++ modules/hcbf_home/templates/hcbf_home.tpl.php
@@ -6,21 +6,27 @@
         <div class="text-center">
           <i class="fa fa-ticket fa-6x"></i>
         </div>
-        <h1 class="text-center">Buy Tickets</h1>
+        <div class="page-header">
+          <h1 class="text-center">Buy Tickets</h1>
+        </div>
         <p class="lead text-center">Tickets will be on sale soon, check back for dates!</p>
       </div>
       <div class="col-xs-12 col-sm-4">
         <div class="text-center">
           <i class="fa fa-beer fa-6x"></i>
         </div>
-        <h1 class="text-center">The Festivities</h1>
+        <div class="page-header">
+          <h1 class="text-center">The Festivities</h1>
+        </div>
         <p class="lead text-center">Join us at the High Country Fair Grounds on Saturday, August 30, 2014 from 3:00pm to 7:00 pm to try a varity of craft beer, craft food, educational seminars, and live music.</p>
       </div>
       <div class="col-xs-12 col-sm-4">
         <div class="text-center">
           <i class="fa fa-users fa-6x"></i>
         </div>
-        <h1 class="text-center">Get involved</h1>
+        <div class="page-header">
+          <h1 class="text-center">Get involved</h1>
+        </div>
         <p class="lead text-center">We're looking for volunteers to help us put on the 7<sup>th</sup> annual High Country Beer Fest.</p>
         <div class="text-center">
           <p>
