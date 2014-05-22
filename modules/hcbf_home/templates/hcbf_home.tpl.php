4f252a178778104e50409b62f928515372588645
Zach Seifts <zach.seifts@gmail.com>
2014-05-22 09:30:13 -0400

    Cleaning up markup and adding in videos

--- modules/hcbf_home/templates/hcbf_home.tpl.php
+++ modules/hcbf_home/templates/hcbf_home.tpl.php
@@ -4,30 +4,46 @@
     <div class="row">
       <div class="col-xs-12 col-sm-4">
         <div class="text-center">
-          <i class="fa fa-ticket fa-6x"></i>
+          <i class="fa fa-ticket fa-5x"></i>
         </div>
         <div class="page-header">
           <h1 class="text-center">Buy Tickets</h1>
         </div>
-        <p class="lead text-center">Tickets will be on sale soon, check back for dates!</p>
+        <p>Tickets are available now!</p>
+        <div class="table-responsive">
+          <table class="table ticket-table">
+            <tbody>
+              <tr>
+                <td>$45.00</td>
+                <td>General Admission</td>
+                <td><a class="btn btn-danger btn-sm" disabled="disabled" href="#">Sold out!</a></td>
+              </tr>
+              <tr>
+                <td>$10.00</td>
+                <td>Designated Driver</td>
+                <td><a class="btn btn-primary btn-sm" href="#">Buy now</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <div class="col-xs-12 col-sm-4">
         <div class="text-center">
-          <i class="fa fa-beer fa-6x"></i>
+          <i class="fa fa-beer fa-5x"></i>
         </div>
         <div class="page-header">
           <h1 class="text-center">The Festivities</h1>
         </div>
-        <p class="lead text-center">Join us at the High Country Fair Grounds on Saturday, August 30, 2014 from 3:00pm to 7:00 pm to try a varity of craft beer, craft food, educational seminars, and live music.</p>
+        <p class="text-center">Join us at the High Country Fair Grounds in Boone, North Carolina on Saturday, August 30, 2014 from 3:00 pm to 7:00 pm to sample a varity of craft beer, craft food, educational seminars, and live music.</p>
       </div>
       <div class="col-xs-12 col-sm-4">
         <div class="text-center">
-          <i class="fa fa-users fa-6x"></i>
+          <i class="fa fa-users fa-5x"></i>
         </div>
         <div class="page-header">
           <h1 class="text-center">Get involved</h1>
         </div>
-        <p class="lead text-center">We're looking for volunteers to help us put on the 7<sup>th</sup> annual High Country Beer Fest.</p>
+        <p class="text-center">We're looking for volunteers to help us put on the 7<sup>th</sup> annual High Country Beer Fest.</p>
         <div class="text-center">
           <p>
             <a class="btn btn-success btn-lg" href="/volunteer" role="button">
@@ -40,21 +56,34 @@
   </div>
 </div>
     
-<hr />
+<div class="wrapper inverse-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8">
+        <div class="video-wrapper">
+          <iframe src="//player.vimeo.com/video/95663874" width="100%" height="420" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <h1 class="heading">The High Country Beer Fest is a celebration of craft beer from around the region and the world.</h1>
+        <p>Come sample outstanding beers and delicious food in the beautiful high country of North Carolina. Educational seminars will cover aspects of beer tasting, brewing, and food pairing.</p>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div id="about-wrapper" class="wrapper">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-md-7">
-        <h1 class="heading">About the High Country Beer Fest</h1>
-        <p class="lead">The High Country Beer Fest is a celebration of craft beer from around the region and the world. Come sample outstanding beers and delicious food in the beautiful high country of North Carolina. Educational seminars will cover aspects of beer tasting, brewing, and food pairing.</p>
+      <div class="col-xs-12 col-md-6">
+        <h1 class="heading text-center">Want to serve your beer?</h1>
+        <p class="lead text-center">We're looking for craft breweries who want to participate in this year's High Country Beer Fest.</p>
+        <p class="text-center"><a class="btn btn-success btn-lg" href="/breweries/register" role="button">I'd like to register my brewery</a></p>
       </div>
-      <div class="col-xs-12 col-md-5">
-        <h1 class="heading">Want to serve your beer?</h1>
-        <p class="lead">We're looking for craft breweries who want to participate in this year's High Country Beer Fest.</p>
-        <p>
-          <a class="btn btn-success btn-lg" href="/breweries/register" role="button">I'd like to register my brewery</a>
-        </p>
+      <div class="col-xs-12 col-md-6">
+        <h1 class="heading text-center">Do you want to be a sponsor?</h1>
+        <p class="lead text-center">Do you want to be a sponsor? We have plenty of sponsorship oppertunities, contact us and we'll let you know how you can help out.</p>
+        <p class="text-center"><a class="btn btn-success btn-lg" href="/sponsors/register" role="button">I'd like to sponsor the Beer Fest</a></p>
       </div>
     </div>
   </div>
