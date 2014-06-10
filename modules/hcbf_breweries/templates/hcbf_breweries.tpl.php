e8e67d4ee5eb2bfde62271f6c1d6454504a46619
Zach Seifts <zach.seifts@gmail.com>
2014-06-10 15:35:03 -0400

    Updating the Breweries page

--- modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
@@ -8,13 +8,12 @@
   <div class="container">
     <div class="col-xs-12">
       <?php foreach ($breweries as $breweries_row) { ?>
-        <div class="row">
+        <div class="row brewery-row">
           <?php foreach ($breweries_row as $brewery) { ?>
             <div class="col-sm-4">
-              <img src="http://placehold.it/220x220" class="img-circle" />
+              <?php print render($brewery['logo']); ?>
               <?php print render($brewery['title']); ?>
               <?php print render($brewery['location']); ?>
-              <?php print render($brewery['website']); ?>
             </div>
           <?php }; ?>
         </div>
