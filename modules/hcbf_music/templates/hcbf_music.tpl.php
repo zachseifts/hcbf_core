5ce975a2b1d5591ad32c6fb0a2c9f208d4f4200a
Zach Seifts <zach.seifts@gmail.com>
2014-06-20 19:13:20 -0400

    Rewriting the music module into a feature
    Be sure to disable and uninstall the hcbf_music module before updating to this
    patch. To do this run:
    
        drush dis -y hcbf_music
        drush pm-uninstall hcbf_music
    
    Then re-enabled the feature in the features admin menu.

--- modules/hcbf_music/templates/hcbf_music.tpl.php
+++ modules/hcbf_music/templates/hcbf_music.tpl.php
@@ -1,34 +1,21 @@
-
+<?php foreach ($bands as $band) { ?>
 <div class="wrapper">
   <div class="container">
     <div class="row">
-      <h2>Jimbo Mathus and The Tri-State Coalition</h2>
+      <div class="col-xs-12">
+        <?php print render($band['title']); ?>
+      </div>
     </div>
     <div class="row">
-      <div class="col-xs-3">
+      <div class="col-xs-12 col-sm-3">
         <img src="http://placehold.it/250x250" />
       </div>
-      <div class="col-xs-9">
-        <p class="lead">The late Memphis producer Jim Dickinson once called Jimbo Mathus “the singing voice of Huck Finn.” Outside the South, Mathus is likely known as the ringleader of the hyper-ragtime outfit Squirrel Nut Zippers.</p>
-        <p>In his native Mississippi and throughout the South, however, Mathus is the prolific songwriter of born‐in‐the‐bone Southern music, the torchbearer for Deep South mythology and culture. Think Delta highways, bowling‐pin Budweisers and “catfish music” for the masses.</p>
+      <div class="col-xs-12 col-sm-9">
+        <?php print render($band['description']); ?>
+        <?php print render($band['website']); ?>
       </div>
     </div>
   </div>
 </div>
+<?php }; ?>
 
-<div class="wrapper">
-  <div class="container">
-    <div class="row">
-      <h2>Jimbo Mathus and The Tri-State Coalition</h2>
-    </div>
-    <div class="row">
-      <div class="col-xs-3">
-        <img src="http://placehold.it/250x250" />
-      </div>
-      <div class="col-xs-9">
-        <p class="lead">The late Memphis producer Jim Dickinson once called Jimbo Mathus “the singing voice of Huck Finn.” Outside the South, Mathus is likely known as the ringleader of the hyper-ragtime outfit Squirrel Nut Zippers.</p>
-        <p>In his native Mississippi and throughout the South, however, Mathus is the prolific songwriter of born‐in‐the‐bone Southern music, the torchbearer for Deep South mythology and culture. Think Delta highways, bowling‐pin Budweisers and “catfish music” for the masses.</p>
-      </div>
-    </div>
-  </div>
-</div>
