44e7de3960ed9fa324c9d5f08a0d08c49c23000c
Zach Seifts <zach.seifts@gmail.com>
2014-06-20 08:26:07 -0400

    Adding in music pages and starting the foods module

--- modules/hcbf_music/templates/hcbf_music.tpl.php
+++ modules/hcbf_music/templates/hcbf_music.tpl.php
@@ -1,6 +1,34 @@
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
+
+<div class="wrapper">
+  <div class="container">
+    <div class="row">
+      <h2>Jimbo Mathus and The Tri-State Coalition</h2>
+    </div>
+    <div class="row">
+      <div class="col-xs-3">
+        <img src="http://placehold.it/250x250" />
+      </div>
+      <div class="col-xs-9">
+        <p class="lead">The late Memphis producer Jim Dickinson once called Jimbo Mathus “the singing voice of Huck Finn.” Outside the South, Mathus is likely known as the ringleader of the hyper-ragtime outfit Squirrel Nut Zippers.</p>
+        <p>In his native Mississippi and throughout the South, however, Mathus is the prolific songwriter of born‐in‐the‐bone Southern music, the torchbearer for Deep South mythology and culture. Think Delta highways, bowling‐pin Budweisers and “catfish music” for the masses.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="wrapper">
+  <div class="container">
+    <div class="row">
+      <h2>Jimbo Mathus and The Tri-State Coalition</h2>
+    </div>
+    <div class="row">
+      <div class="col-xs-3">
+        <img src="http://placehold.it/250x250" />
+      </div>
+      <div class="col-xs-9">
+        <p class="lead">The late Memphis producer Jim Dickinson once called Jimbo Mathus “the singing voice of Huck Finn.” Outside the South, Mathus is likely known as the ringleader of the hyper-ragtime outfit Squirrel Nut Zippers.</p>
+        <p>In his native Mississippi and throughout the South, however, Mathus is the prolific songwriter of born‐in‐the‐bone Southern music, the torchbearer for Deep South mythology and culture. Think Delta highways, bowling‐pin Budweisers and “catfish music” for the masses.</p>
+      </div>
     </div>
   </div>
 </div>
