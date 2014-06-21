f174d3f16e8330caadb90d5e20ce6332e94d90cf
Zach Seifts <zach.seifts@gmail.com>
2014-06-20 20:11:58 -0400

    Adding in the hcbf_about module

---
+++ modules/hcbf_about/templates/hcbf_about.tpl.php
@@ -0,0 +1,43 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>The High Country Beer Fest is a celebration of craft beer from around the region and the world.</h1>
+  </div>
+</div>
+
+<div class="wrapper inverse-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1>Join us for the 7<sup>th</sup> annual High Country Beer Fest at the High Country Fairgrounds. Sample craft beers, craft food, music, and seminars in the high country of North Carolina. Our seminars will cover aspects of beer tasting, brewing, and food pairing.</h1>
+        <p class="lead">Our 2014 non-profits include the <a href="http://www.highcountrylocalfirst.org/" title="High Country Local First">High Country Local First</a>, <a href="http://www.highcountrymommies.com/" title="High Country Mommies">High Country Mommies</a>, and Ivory Tower, Inc. All of these organizations will receive a portion of the proceeds of this year's High Country Beer Fest.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <div class="text-center">
+          <i class="icon fa fa-map-marker fa-5x"></i>
+        </div>
+        <h2>We have a new location</h3>
+        <p>This year we're expanding the Beer Fest and moving to a new location, the High Country Fairgrounds. Get ready for more music, breweries, food, and seminars!</p>
+        <a class="btn btn-success" href="/about/location" role="button">
+          <i class="fa fa-taxi"></i> Get there
+        </a>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <div class="text-center">
+          <i class="icon fa fa-taxi fa-5x"></i>
+        </div>
+        <h2>What's your exit strategy?</h3>
+        <p>We want to make sure have you have a great time at our event, and get home safely. We've teamed up with local businesses to help you find a way home.</p>
+        <a class="btn btn-success" href="/about/exit" role="button">
+          <i class="fa fa-taxi"></i> Get home
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ modules/hcbf_about/templates/hcbf_about_exit.tpl.php
@@ -0,0 +1,18 @@
+<div class="wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-4">
+        <p class="lead">The High Country Craft Food and Beverage Festival wants to make sure that you have a great time at the Beer Fest, but we also want to make sure you get home safely.</p>
+      </div>
+      <div class="col-xs-12 col-md-8">
+        <ul>
+          <li>Bill's Garage will tow your vehicle and up to 4 passengers within the Boone City Limits, FOR FREE!</li>
+          <li>Tipsy Taxi is providing FREE rides anywhere in Boone.</li>
+          <li>Designated Driver Tickets are only $10</li>
+        </ul>
+
+        <p>With so many options there's no excuse for getting behind the wheel when you've enjoyed yourself a little too much.</p>
+      </div>
+    </div>
+  </div>
+</div>
