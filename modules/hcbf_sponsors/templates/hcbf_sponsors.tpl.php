f73d4263593445da7f1ed9c072ca08d8b0f8e097
Zach Seifts <zach.seifts@gmail.com>
2014-07-07 09:20:27 -0400

    Adding in sponsors

--- modules/hcbf_sponsors/templates/hcbf_sponsors.tpl.php
+++ modules/hcbf_sponsors/templates/hcbf_sponsors.tpl.php
@@ -1,12 +1,76 @@
-<div class="sponsor-wrapper">
+<div class="sponsors-wrapper">
   <div class="container">
     <div class="row">
-      <div class="col-sm-6">
-        <p>This is left side of the sponsors page</p>
-      </div>
-      <div class="col-sm-6">
-        <p>This is the right side of the sponsors page</p>
+      <div class="col-xs-12">
+        <?php foreach ($level_4_sponsors as $sponsors_row) { ?>
+          <div class="row sponsors-row">
+            <?php foreach ($sponsors_row as $sponsor) { ?>
+              <div class="col-sm-6">
+                <?php print render($sponsor['logo']); ?>
+                <?php print render($sponsor['title']); ?>
+              </div>
+            <?php }; ?>
+          </div>
+        <?php }; ?>
       </div>
     </div>
   </div>
 </div>
+
+<div class="sponsors-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php foreach ($level_3_sponsors as $sponsors_row) { ?>
+          <div class="row sponsors-row">
+            <?php foreach ($sponsors_row as $sponsor) { ?>
+              <div class="col-sm-6">
+                <?php print render($sponsor['logo']); ?>
+                <?php print render($sponsor['title']); ?>
+              </div>
+            <?php }; ?>
+          </div>
+        <?php }; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="sponsors-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php foreach ($level_2_sponsors as $sponsors_row) { ?>
+          <div class="row sponsors-row">
+            <?php foreach ($sponsors_row as $sponsor) { ?>
+              <div class="col-sm-6">
+                <?php print render($sponsor['logo']); ?>
+                <?php print render($sponsor['title']); ?>
+              </div>
+            <?php }; ?>
+          </div>
+        <?php }; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="sponsors-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php foreach ($level_1_sponsors as $sponsors_row) { ?>
+          <div class="row sponsors-row">
+            <?php foreach ($sponsors_row as $sponsor) { ?>
+              <div class="col-sm-6">
+                <?php print render($sponsor['logo']); ?>
+                <?php print render($sponsor['title']); ?>
+              </div>
+            <?php }; ?>
+          </div>
+        <?php }; ?>
+      </div>
+    </div>
+  </div>
+</div>
+
