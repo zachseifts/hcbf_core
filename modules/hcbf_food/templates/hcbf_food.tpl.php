f38cd2f38738bc82845a58253e1f9fd7a672d326
Zach Seifts <zach.seifts@gmail.com>
2014-08-01 13:57:09 -0400

    Updating the vendors page

--- modules/hcbf_food/templates/hcbf_food.tpl.php
+++ modules/hcbf_food/templates/hcbf_food.tpl.php
@@ -1,22 +1,37 @@
-<?php foreach ($foods as $food) { ?>
-<div class="wrapper">
+<?php
+/**
+ * @file
+ * A page that displays the food vendors at the Beer Fest.
+ */
+?>
+
+<div class="food-wrapper">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12">
-        <?php print render($food['title']); ?>
+      <div class="col-xs-12 col-sm-6 col-lg-8">
+        <div class="container-fluid">
+          <?php foreach ($foods as $food) { ?>
+            <div class="row">
+              <div class="col-xs-12">
+                <?php print render($food['title']); ?>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-3">
+                <?php print render($food['image']); ?>
+                <p><?php print render($food['website']); ?></p>
+              </div>
+              <div class="col-xs-12 col-sm-9">
+                <?php print render($food['description']); ?>
+              </div>
+            </div>
+          <?php }; ?>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-3">
-        <?php print render($food['image']); ?>
-      </div>
-      <div class="col-xs-12 col-sm-9">
-        <?php print render($food['description']); ?>
-        <?php print render($food['website']); ?>
+      <div class="col-xs-12 col-sm-6 col-lg-4">
+        <?php print render(hcbf_buy_tickets_box(array('well'))); ?>
       </div>
     </div>
   </div>
 </div>
-<?php }; ?>
-
 
