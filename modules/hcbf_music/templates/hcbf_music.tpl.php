758aea5fa06f94224668a75358b85550d2231355
Zach Seifts <zach.seifts@gmail.com>
2014-08-01 13:51:55 -0400

    Updating the music page's layout

--- modules/hcbf_music/templates/hcbf_music.tpl.php
+++ modules/hcbf_music/templates/hcbf_music.tpl.php
@@ -1,28 +1,37 @@
 <?php
 /**
+ * @file
  * A page that displays the musical acts at the Beer Fest.
  */
 ?>
 
-
-<?php foreach ($bands as $band) { ?>
-<div class="wrapper">
+<div class="music-wrapper">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12">
-        <?php print render($band['title']); ?>
+      <div class="col-xs-12 col-sm-6 col-lg-8">
+        <div class="container-fluid">
+          <?php foreach ($bands as $band) { ?>
+            <div class="row">
+              <div class="col-xs-12">
+                <?php print render($band['title']); ?>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-3">
+                <?php print render($band['image']); ?>
+                <p><?php print render($band['website']); ?></p>
+              </div>
+              <div class="col-xs-12 col-sm-9">
+                <?php print render($band['description']); ?>
+              </div>
+            </div>
+          <?php }; ?>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-3">
-        <?php print render($band['image']); ?>
-      </div>
-      <div class="col-xs-12 col-sm-9">
-        <?php print render($band['description']); ?>
-        <?php print render($band['website']); ?>
+      <div class="col-xs-12 col-sm-6 col-lg-4">
+        <?php print render(hcbf_buy_tickets_box(array('well'))); ?>
       </div>
     </div>
   </div>
 </div>
-<?php }; ?>
 
