ac99404214662517ef58e55e42b0f8bedbb1beb7
Zach Seifts <zach.seifts@gmail.com>
2014-08-01 10:43:37 -0400

    Creating a block for the buy tickets now box

--- modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries.tpl.php
@@ -6,18 +6,27 @@
 
 <div class="breweries-wrapper">
   <div class="container">
-    <div class="col-xs-12">
-      <?php foreach ($breweries as $breweries_row) { ?>
-        <div class="row brewery-row">
-          <?php foreach ($breweries_row as $brewery) { ?>
-            <div class="col-sm-4">
-              <?php print render($brewery['logo']); ?>
-              <?php print render($brewery['title']); ?>
-              <?php print render($brewery['location']); ?>
+    <div class="row">
+      <div class="col-xs-12 col-md-9">
+        <div class="container-fluid">
+
+          <?php foreach ($breweries as $breweries_row) { ?>
+            <div class="row brewery-row">
+              <?php foreach ($breweries_row as $brewery) { ?>
+                <div class="col-sm-12 col-md-4">
+                  <?php print render($brewery['logo']); ?>
+                  <?php print render($brewery['title']); ?>
+                  <?php print render($brewery['location']); ?>
+                </div>
+              <?php }; ?>
             </div>
           <?php }; ?>
+
         </div>
-      <?php }; ?>
+      </div>
+      <div class="col-xs-12 col-md-3">
+        <?php print render(hcbf_buy_tickets_box(array('well'))); ?>
+      </div>
     </div>
   </div>
 </div>
