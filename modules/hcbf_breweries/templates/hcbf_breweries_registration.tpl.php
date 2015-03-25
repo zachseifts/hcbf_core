c6ed2e7ca37f0a0b5dfa95b1fa473f76e0d67662
Zach Seifts <zach.seifts@gmail.com>
2015-03-25 11:26:49 -0400

    Adding in messages

--- modules/hcbf_breweries/templates/hcbf_breweries_registration.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries_registration.tpl.php
@@ -7,12 +7,16 @@
 
 <div class="register-brewery-wrapper">
   <h1 class="heading text-center">Want to server your beer?</h1>
-  <p class="lead text-center">
-    We'd love to have your beer! We're looking for craft breweries who want to participate in this year's High Country Beer Fest.
-  </p>
-  <p class="text-center">
-    <a href="/breweries/register" class="btn-success btn btn-lg">
-      I'd like to register my brewery
-    </a>
-  </p>
+  <?php if (variable_get('hcbf_breweries_active', 0) == 1): ?>
+    <p class="lead text-center">
+      We'd love to have your beer! We're looking for craft breweries who want to participate in this year's High Country Beer Fest.
+    </p>
+    <p class="text-center">
+      <a href="/breweries/register" class="btn-success btn btn-lg">
+        I'd like to register my brewery
+      </a>
+    </p>
+  <?php else: ?>
+    <p class="lead text-center">We're not accepting brewery registrations at the moment, check back soon!</p>
+  <?php endif; ?>
 </div>
