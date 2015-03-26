e616d2e19c10c07b04218fbac6d32b15e01df466
Zach Seifts <zach.seifts@gmail.com>
2015-03-26 15:51:53 -0400

    Fixing php errors

--- modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
@@ -1,22 +1,27 @@
 <div class="breweries">
   <h2>Current breweries</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Location</th>
-        <th>&nbsp;</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($breweries as $brewery): ?>
-      <tr>
+
+  <?php if (empty($breweries)): ?>
+    <h3>There are no breweries signed up yet.</h3>
+  <?php else: ?>
+    <table>
+      <thead>
         <tr>
-          <td><?php print l($brewery['name'], 'node/' . $brewery['nid']); ?></td>
-          <td><?php print $brewery['location']; ?></td>
-          <td><?php print l(t('Edit'), 'node/' . $brewery['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/breweries'))); ?></td>
+          <th>Name</th>
+          <th>Location</th>
+          <th>&nbsp;</th>
         </tr>
-      </tr>
-      <?php endforeach; ?>
-  </table>
+      </thead>
+      <tbody>
+        <?php foreach ($breweries as $brewery): ?>
+        <tr>
+          <tr>
+            <td><?php print l($brewery['name'], 'node/' . $brewery['nid']); ?></td>
+            <td><?php print $brewery['location']; ?></td>
+            <td><?php print l(t('Edit'), 'node/' . $brewery['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/breweries'))); ?></td>
+          </tr>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+  <?php endif; ?>
 </div>
