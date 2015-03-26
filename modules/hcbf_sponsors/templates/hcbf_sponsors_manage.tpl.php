c54003f44986b6a4e96194972109c7b8d0465c6e
Zach Seifts <zach.seifts@gmail.com>
2015-03-26 13:57:14 -0400

    Adding in sponsor management pages

---
+++ modules/hcbf_sponsors/templates/hcbf_sponsors_manage.tpl.php
@@ -0,0 +1,25 @@
+<div class="sponsors">
+  <h2>Current sponsors</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th colspan="3">&nbsp;</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <?php foreach ($sponsors as $sponsor): ?>
+      <tr>
+        <tr>
+          <td><?php print $sponsor['name']; ?></td>
+          <td><?php print $sponsor['view']; ?></td>
+          <td><?php print $sponsor['edit']; ?></td>
+          <td><?php print $sponsor['delete']; ?></td>
+        </tr>
+      </tr>
+      <?php endforeach; ?>
+
+  </table>
+</div>
+
