e6ffcc364345dae1a95eeb9bd87d07706fa6c43c
Zach Seifts <zach.seifts@gmail.com>
2015-03-26 14:40:44 -0400

    Reworking the breweries management page a bit

--- modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
@@ -5,24 +5,18 @@
       <tr>
         <th>Name</th>
         <th>Location</th>
-        <th>Accepted</th>
-        <th colspan="3">&nbsp;</th>
+        <th>&nbsp;</th>
       </tr>
     </thead>
     <tbody>
-
       <?php foreach ($breweries as $brewery): ?>
       <tr>
         <tr>
-          <td><?php print $brewery['name']; ?></td>
+          <td><?php print l($brewery['name'], 'node/' . $brewery['nid']); ?></td>
           <td><?php print $brewery['location']; ?></td>
-          <td><?php print $brewery['accepted']; ?></td>
-          <td><?php print $brewery['view']; ?></td>
-          <td><?php print $brewery['edit']; ?></td>
-          <td><?php print $brewery['delete']; ?></td>
+          <td><?php print l(t('Edit'), 'node/' . $brewery['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/breweries'))); ?></td>
         </tr>
       </tr>
       <?php endforeach; ?>
-
   </table>
 </div>
