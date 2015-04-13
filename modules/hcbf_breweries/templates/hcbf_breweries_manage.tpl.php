c1d7a615f249074d71f8f9f201852a21f4bcf12a
Zach Seifts <zach.seifts@gmail.com>
2015-04-13 14:20:41 -0400

    Adding in a listing of all the breweries to the brewery management page

--- modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
+++ modules/hcbf_breweries/templates/hcbf_breweries_manage.tpl.php
@@ -25,6 +25,31 @@
     </table>
   <?php endif; ?>
 
+  <h2>All breweries</h2>
+
+  <?php if (empty($all_breweries)): ?>
+    <h3>There are no breweries signed up yet.</h3>
+  <?php else: ?>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($all_breweries as $brewery): ?>
+        <tr>
+          <tr>
+            <td><?php print l($brewery['name'], 'node/' . $brewery['nid']); ?></td>
+            <td><?php print $brewery['location']; ?></td>
+            <td><?php print l(t('Edit'), 'node/' . $brewery['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/breweries'))); ?></td>
+          </tr>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+  <?php endif; ?>
   <ul class="action-links">
     <li><?php print l(t('Add a brewery'), 'node/add/hcbf-brewery'); ?></li>
   </ul>
