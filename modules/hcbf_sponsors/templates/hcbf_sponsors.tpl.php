2155c0ebd41ff82e4b1cd0d6d054b1ed7c8c0260
Zach Seifts <zach.seifts@gmail.com>
2015-04-15 13:19:54 -0400

    Changing up how sponsors pages are rendered

--- modules/hcbf_sponsors/templates/hcbf_sponsors.tpl.php
+++ modules/hcbf_sponsors/templates/hcbf_sponsors.tpl.php
@@ -10,79 +10,44 @@
     <div class="row">
       <div class="col-sm-12 col-lg-8">
         <div class="container-fluid">
+          <?php if (empty($sponsors)): ?>
+            <h1>We're announcing sponsors soon!</h1>
+          <?php endif; ?>
 
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="page-header">
-                <h2>Our Imperial Stout level sponsors</h2>
+          <?php foreach ($sponsors as $level => $level_sponsors): ?>
+
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="page-header">
+                  <h2><?php print $level; ?> Sponsors</h2>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row sponsors-row">
-            <?php foreach ($level_4_sponsors as $sponsors_row) { ?>
+
+            <div class="row sponsors-row">
+              <?php $level_sponsors = array_chunk($level_sponsors, 2); ?>
+              <?php foreach ($level_sponsors as $sponsor): ?>
+                <?php foreach ($sponsor as $s): ?>
+                  <div class="col-sm-12 col-sm-6">
+                    <div class="text-center">
+                      <?php print $s['logo']; ?>
+                    </div>
+                    <h2 class="text-center">
+                      <?php print l($s['name'], $s['link']); ?>
+                    </h2>
+                  </div>
+                <?php endforeach; ?>
+              <?php endforeach; ?>
+
               <?php foreach ($sponsors_row as $sponsor) { ?>
                 <div class="col-sm-12 col-sm-6">
                   <?php print render($sponsor['logo']); ?>
                   <?php print render($sponsor['title']); ?>
                 </div>
               <?php }; ?>
-            <?php }; ?>
-          </div>
-
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="page-header">
-                <h2>Our Double IPA level sponsors</h2>
-              </div>
             </div>
-          </div>
-          <div class="row sponsors-row">
-            <?php foreach ($level_3_sponsors as $sponsors_row) { ?>
-              <?php foreach ($sponsors_row as $sponsor) { ?>
-                <div class="col-sm-4">
-                  <?php print render($sponsor['logo']); ?>
-                  <?php print render($sponsor['title']); ?>
-                </div>
-              <?php }; ?>
-            <?php }; ?>
-          </div>
 
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="page-header">
-                <h2>Our Pale Ale level sponsors</h2>
-              </div>
-            </div>
-          </div>
-          <div class="row sponsors-row">
-            <?php foreach ($level_2_sponsors as $sponsors_row) { ?>
-              <?php foreach ($sponsors_row as $sponsor) { ?>
-                <div class="col-sm-4">
-                  <?php print render($sponsor['logo']); ?>
-                  <?php print render($sponsor['title']); ?>
-                </div>
-              <?php }; ?>
-            <?php }; ?>
-          </div>
-
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="page-header">
-                <h2>Our Session Ale level sponsors</h2>
-              </div>
-            </div>
-          </div>
-          <div class="row sponsors-row">
-            <?php foreach ($level_1_sponsors as $sponsors_row) { ?>
-              <?php foreach ($sponsors_row as $sponsor) { ?>
-                <div class="col-sm-4">
-                  <?php print render($sponsor['logo']); ?>
-                  <?php print render($sponsor['title']); ?>
-                </div>
-              <?php }; ?>
-            <?php }; ?>
-          </div>
-
+          <?php endforeach; ?>
         </div>
       </div>
       <div class="col-sm-12 col-lg-4">
@@ -98,4 +63,3 @@
     </div>
   </div>
 </div>
-
--- modules/hcbf_sponsors/templates/hcbf_sponsors_manage.tpl.php
+++ modules/hcbf_sponsors/templates/hcbf_sponsors_manage.tpl.php
@@ -1,25 +1,59 @@
 <div class="sponsors">
   <h2>Current sponsors</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th colspan="3">&nbsp;</th>
-      </tr>
-    </thead>
-    <tbody>
+  <?php if (empty($sponsors)): ?>
+    <h3>There are no sponsors signed up yet.</h3>
+  <?php else: ?>
+    <?php foreach ($sponsors as $level => $level_sponsors): ?>
+      <h4><?php print $level; ?></h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($level_sponsors as $sponsor): ?>
+          <tr>
+            <tr>
+              <td><?php print $sponsor['name']; ?></td>
+              <td><?php print l(t('Edit'), 'node/' . $sponsor['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/sponsors'))); ?></td>
+            </tr>
+          </tr>
+        <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php endforeach; ?>
+  <?php endif; ?>
 
-      <?php foreach ($sponsors as $sponsor): ?>
-      <tr>
-        <tr>
-          <td><?php print $sponsor['name']; ?></td>
-          <td><?php //print $sponsor['view']; ?></td>
-          <td><?php //print $sponsor['edit']; ?></td>
-          <td><?php //print $sponsor['delete']; ?></td>
-        </tr>
-      </tr>
-      <?php endforeach; ?>
+  <h2>All sponsors</h2>
+  <?php if (empty($all_sponsors)): ?>
+    <h3>There are no sponsors signed up yet.</h3>
+  <?php else: ?>
+    <?php foreach ($all_sponsors as $level => $level_sponsors): ?>
+      <h4><?php print $level; ?></h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($level_sponsors as $sponsor): ?>
+          <tr>
+            <tr>
+              <td><?php print $sponsor['name']; ?></td>
+              <td><?php print l(t('Edit'), 'node/' . $sponsor['nid'] . '/edit', array('query' => array('destination' => 'admin/config/hcbf/sponsors'))); ?></td>
+            </tr>
+          </tr>
+        <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php endforeach; ?>
+  <?php endif; ?>
+  <ul class="action-links">
+    <li><?php print l(t('Add a sponsor'), 'node/add/hcbf-sponsor'); ?></li>
+  </ul>
 
-  </table>
 </div>
-
