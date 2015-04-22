79198322e6d8ba6d3d04e5a7105211863656a9de
Zach Seifts <zach.seifts@gmail.com>
2015-04-22 11:17:45 -0400

    Fixing tickets block

--- modules/hcbf_festival/templates/hcbf_festival_tickets.tpl.php
+++ modules/hcbf_festival/templates/hcbf_festival_tickets.tpl.php
@@ -14,33 +14,35 @@
     <h2 class="text-center">Buy Tickets</h2>
   </div>
   
-  <p class="lead">Tickets will be on sale soon!</p>
+  <?php if (variable_get('hcbf_core_on_sale', 0) != 1): ?>
+    <p class="lead text-center">Tickets will be on sale soon!</p>
+  <?php else: ?>
+    <div class="table-responsive">
+      <table class="table ticket-table">
+        <tbody>
+          <tr>
+            <td>General Admission</td>
+            <td>
+              <a href="<?php print variable_get('hcbf_core_regular_ticket_link', '#'); ?>" class="btn btn-success btn-sm">
+                $<?php print variable_get('hcbf_core_regular_ticket_price', '40.00'); ?>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>Designated Driver</td>
+            <td>
+              <a href="<?php print variable_get('hcbf_core_dd_ticket_link', '#'); ?>" class="btn btn-success btn-sm">
+                $<?php print variable_get('hcbf_core_dd_ticket_price', '10.00'); ?>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  <?php endif; ?>
+
   <?php // TODO: Remove this hacky line soon. ?>
   <p>&nbsp;</p>
   
-  <!--
-  <div class="table-responsive">
-    <table class="table ticket-table">
-      <tbody>
-        <tr>
-          <td>General Admission</td>
-          <td>
-            <a href="<?php print variable_get('hcbf_core_regular_ticket_link', '#'); ?>" class="btn btn-danger btn-sm">
-              $<?php print variable_get('hcbf_core_regular_ticket_price', '40.00'); ?>
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>Designated Driver</td>
-          <td>
-            <a href="<?php print variable_get('hcbf_core_dd_ticket_link', '#'); ?>" class="btn btn-danger btn-sm">
-              $<?php print variable_get('hcbf_core_dd_ticket_price', '10.00'); ?>
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  -->
 </div>
 
